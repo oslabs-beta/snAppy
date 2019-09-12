c8f62c523099e4b06ff2f6dde1b35038e0bebb82
Jackie Lin <jackie.lin128@gmail.com>
2019-09-12 15:39:55 -0400

    created a webview panel
    Co-authored-by: courtneywkwong <cwkwong95@gmail.com>
    Co-authored-by: vinetree4u <vinetree@gmail.com>
    Co-authored-by: jackiel12 <jackie.lin128@gmail.com>
    Co-authored-by: onaumova <leliknaum@gmail.com>

--- nimble/src/extension.ts
+++ nimble/src/extension.ts
@@ -1,27 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {URI} from 'vscode-uri';
+const path = require('path');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "nimble" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let startCommand = vscode.commands.registerCommand('extension.startNimble', () => {
+		
+		const panel = vscode.window.createWebviewPanel('nimble', 'Nimble', vscode.ViewColumn.Beside, 
+		{
+			enableScripts: true,
+		});
+	panel.webview.html = getWebviewContent();
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(startCommand);
 }
 
-// this method is called when your extension is deactivated
+function getWebviewContent() {
+	const nonce = getNonce();
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';">
+		<title>Nimble</title>
+	</head>
+	<body>
+	<script nonce="${nonce}">
+	const vscode = acquireVsCodeApi();
+	</script>
+	</body>
+	</html>`
+}
+
+function getNonce() {
+	let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 export function deactivate() {}
