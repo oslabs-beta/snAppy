46339fa7a3ac9d31b8c5434448e90cfae2880bd8
courtneywkwong <cwkwong95@gmail.com>
2019-09-14 14:07:25 -0400

    fixed tsconfigs - added tsconfig to view structure

--- nimble/out/extension.js
+++ nimble/out/extension.js
@@ -1,83 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-const vscode_uri_1 = require("vscode-uri");
-// import { TextEncoder } from 'util';
-const path = require('path');
+// import * as vscode from 'vscode';
+const vscode_1 = require("vscode");
+// const path = require('path');
+//this is the new code we copied from aliens
+const path = require("path");
+function loadScript(context, path) {
+    return `<script src="${vscode_1.Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource' }).toString()}"></script>`;
+}
 function activate(context) {
     console.log('Congratulations, your extension "nimble" is now active!');
-    let startCommand = vscode.commands.registerCommand('extension.startNimble', () => {
-        const panel = vscode.window.createWebviewPanel('nimble', 'Nimble', vscode.ViewColumn.Beside, {
+    let startCommand = vscode_1.commands.registerCommand('extension.startNimble', () => {
+        const panel = vscode_1.window.createWebviewPanel('nimble', 'Nimble', vscode_1.ViewColumn.Beside, {
             enableScripts: true,
+            retainContextWhenHidden: true,
+            localResourceRoots: [vscode_1.Uri.file(path.join(context.extensionPath, 'out'))]
         });
-        panel.webview.html = getWebviewContent();
-        function readFile(read, write) {
-            console.log('reading?');
-            let URIto = vscode_uri_1.URI.file(write);
-            let URIfrom = vscode_uri_1.URI.file(read);
-            vscode.workspace.fs.readFile(URIfrom)
-                .then(res => {
-                // console.log(res.toString())
-                vscode.workspace.fs.writeFile(URIto, res)
-                    .then(data => console.log(data))
-                    .then(undefined, error => console.log('writing error:', error));
-            })
-                .then(undefined, err => console.log('reading error', err));
-        }
-        let readURI = '/Users/courtneykwong/Documents/Codesmith/Projects/samples/vscExt/helloworld/src/sampleTest.html';
-        let writeURI = '/Users/courtneykwong/Documents/Codesmith/Projects/samples/vscExt/helloworld/src/new.html';
-        //this is an api function that speaks from ext to webview/recieving and doing sonething after
-        panel.webview.onDidReceiveMessage(message => {
-            switch (message.command) {
-                case 'alert':
-                    readFile(readURI, writeURI);
-                    vscode.window.showInformationMessage(message.text);
-                    vscode.window.showInformationMessage('is it really tho');
-            }
-        });
+        panel.webview.html = getWebviewContent(context);
     });
     context.subscriptions.push(startCommand);
 }
 exports.activate = activate;
-function getWebviewContent() {
-    const nonce = getNonce();
+function getWebviewContent(context) {
     return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';">
 		<title>Nimble</title>
 	</head>
 
 	<body>
-	<h2>The Button Element</h2>
-	<button id='test'>Use</button>
-
-	<script nonce="${nonce}">
-
-	const vscode = acquireVsCodeApi();
-
-	const commandHandler = () => {
-		console.log('IM HERE')
-		vscode.postMessage({
-			command: 'alert',
-			text: 'its working!'
-		})
-	}
-	document.querySelector('#test').addEventListener('click', commandHandler);
-
-	</script>
+		<div id="root"></div>
+		<p>Hello Jackie</p>
+		${loadScript(context, 'out.js')}
 	</body>
 	</html>`;
-}
-function getNonce() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
 }
 function deactivate() { }
 exports.deactivate = deactivate;
--- nimble/src/extension.ts
+++ nimble/src/extension.ts
@@ -1,89 +1,45 @@
-import * as vscode from 'vscode';
+// import * as vscode from 'vscode';
+import { ExtensionContext, commands, window, ViewColumn, Uri } from 'vscode';
 import {URI} from 'vscode-uri';
-// import { TextEncoder } from 'util';
-const path = require('path');
+// const path = require('path');
+//this is the new code we copied from aliens
+import * as path from 'path';
 
-export function activate(context: vscode.ExtensionContext) {
+function loadScript(context: ExtensionContext, path: string) {
+    return `<script src="${Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource'}).toString()}"></script>`;
+}
+
+export function activate(context: ExtensionContext) {
 	console.log('Congratulations, your extension "nimble" is now active!');
-	let startCommand = vscode.commands.registerCommand('extension.startNimble', () => {
+	let startCommand = commands.registerCommand('extension.startNimble', () => {
 		
-		const panel = vscode.window.createWebviewPanel('nimble', 'Nimble', vscode.ViewColumn.Beside, 
+		const panel = window.createWebviewPanel('nimble', 'Nimble', ViewColumn.Beside, 
 		{
 			enableScripts: true,
+			retainContextWhenHidden: true,
+            localResourceRoots: [ Uri.file(path.join(context.extensionPath, 'out')) ]
 		});
-	panel.webview.html = getWebviewContent();
-	
-	
-	function readFile(read:any, write:any ) {
-		console.log('reading?')
-		let URIto = URI.file(write);
-		let URIfrom = URI.file(read)
-		vscode.workspace.fs.readFile(URIfrom)
-			.then(res => {
-				// console.log(res.toString())
-				vscode.workspace.fs.writeFile(URIto, res)
-					.then(data => console.log(data))
-					.then(undefined, error => console.log('writing error:', error))
-			})
-			.then(undefined, err => console.log('reading error', err))
-	}
-	let readURI = '/Users/courtneykwong/Documents/Codesmith/Projects/samples/vscExt/helloworld/src/sampleTest.html'
-	let writeURI = '/Users/courtneykwong/Documents/Codesmith/Projects/samples/vscExt/helloworld/src/new.html'
-	//this is an api function that speaks from ext to webview/recieving and doing sonething after
-	panel.webview.onDidReceiveMessage(
-		message => {
-			switch(message.command) {
-				case 'alert':
-					readFile(readURI, writeURI) 
-					vscode.window.showInformationMessage(message.text)
-					vscode.window.showInformationMessage('is it really tho')
-			}
-		}
-	)
+	panel.webview.html = getWebviewContent(context);
 	});
 	context.subscriptions.push(startCommand);
 }
 
-function getWebviewContent() {
-	const nonce = getNonce();
+function getWebviewContent(context: ExtensionContext) {
 	return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';">
 		<title>Nimble</title>
 	</head>
 
 	<body>
-	<h2>The Button Element</h2>
-	<button id='test'>Use</button>
-
-	<script nonce="${nonce}">
-
-	const vscode = acquireVsCodeApi();
-
-	const commandHandler = () => {
-		console.log('IM HERE')
-		vscode.postMessage({
-			command: 'alert',
-			text: 'its working!'
-		})
-	}
-	document.querySelector('#test').addEventListener('click', commandHandler);
-
-	</script>
+		<div id="root"></div>
+		<p>Hello Jackie</p>
+		${loadScript(context, 'out.js')}
 	</body>
 	</html>`
 }
 
-function getNonce() {
-	let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
 
 export function deactivate() {}
