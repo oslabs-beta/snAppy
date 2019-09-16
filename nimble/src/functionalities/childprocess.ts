a333e01fae7b43bd5fefd10cd0dce7bfb15349d9
courtneywkwong <cwkwong95@gmail.com>
2019-09-15 22:40:31 -0400

    reverted extensions.ts back in root - it needs to be here

--- nimble/src/functionalities/childprocess.ts
+++ nimble/src/functionalities/childprocess.ts
@@ -2,4 +2,5 @@
 const {exec} = require('child_process');
 
 console.log('running child process');
+console.log(__dirname)
 exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
