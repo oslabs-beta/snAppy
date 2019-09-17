9a7b994cb1b2df447152a3dbd438895ea563c99c
lelik <lola@leliks-MacBook-Pro.local>
2019-09-16 20:37:35 -0400

    read and written an extrenal wepackconfig and saved it in the extnsion root folder, example webpack seem to have an error.
    Co-authored-by: courtneywkwong <cwkwong95@gmail.com>

--- nimble/out/extension.js
+++ nimble/out/extension.js
@@ -3,120 +3,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // import * as vscode from 'vscode';
 const vscode_1 = require("vscode");
 //node docs;
-const { exec } = require('child_process');
-// let webpackConfig :any = {
-//     entry: {
-//         aliens: './src/view/index.tsx'
-//     },
-//     output: {
-//         path: path.resolve(__dirname, 'out'),
-//         filename: "[name].js"
-//     },
-//     optimization: {
-//         splitChunks: {
-//             cacheGroups: {
-//                 vendor: {
-//                     test: /node_modules/,
-//                     chunks: "initial",
-//                     name: "vendor",
-//                     priority: 10,
-//                     enforce: true
-//                 }
-//             }
-//         }
-//     },
-//     devtool: 'eval-source-map',
-//     resolve: {
-//         extensions: ['.js', '.ts', '.tsx', '.json']
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.(ts|tsx)$/,
-//                 loader: 'ts-loader',
-//                 options: {
-//                     getCustomTransformers: () => ({
-//                         before: [ tsImportPlugin({
-//                           libraryName: 'antd',
-//                           libraryDirectory: 'es',
-//                           style: true,
-//                         }) ]
-//                      })
-//                 }
-//             },
-//             {
-//                 test: /\.less$/,
-//                 use: [
-//                     {
-//                         loader: 'style-loader'
-//                     },
-//                     {
-//                         loader: 'css-loader',
-//                         options: {
-//                             importLoaders: 1,
-//                             sourceMap: true
-//                         }
-//                     },
-//                     {
-//                         loader: 'less-loader',
-//                         options: {
-//                             javascriptEnabled: true,
-//                             sourceMap: true,
-//                             modifyVars: {
-//                                 '@body-background': 'var(--background-color)',
-//                             }
-//                         }
-//                     }
-//                 ]
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     {
-//                         loader: 'style-loader'
-//                     },
-//                     {
-//                         loader: 'css-loader'
-//                     }
-//                 ]
-//             }
-//         ]
-//     },
-//     performance: {
-//         hints: false
-//     }
-// }
+const { exec } = require("child_process");
+const vscode_uri_1 = require("vscode-uri");
 function loadScript(context, path) {
-    return `<script src="${vscode_1.Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource' }).toString()}"></script>`;
+    return `<script src="${vscode_1.Uri.file(context.asAbsolutePath(path))
+        .with({ scheme: "vscode-resource" })
+        .toString()}"></script>`;
 }
 function activate(context) {
     console.log('Congratulations, your extension "nimble" is now active!');
     // exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
-    let startCommand = vscode_1.commands.registerCommand('extension.startNimble', () => {
-        const panel = vscode_1.window.createWebviewPanel('nimble', 'Nimble', vscode_1.ViewColumn.Beside, { enableScripts: true, });
+    let startCommand = vscode_1.commands.registerCommand("extension.startNimble", () => {
+        const panel = vscode_1.window.createWebviewPanel("nimble", "Nimble", vscode_1.ViewColumn.Beside, { enableScripts: true });
         panel.webview.html = getWebviewContent(context);
         panel.webview.onDidReceiveMessage(message => {
             console.log(message.command);
             let moduleState;
             switch (message.command) {
-                case 'config':
-                    console.log('getting input and configuring webpack');
-                    console.log('message', message.module);
+                case "config":
+                    console.log("getting input and configuring webpack");
+                    console.log("message", message.module);
                     moduleState = Object.assign({}, message.module);
-                // let moduleObj = createModule(moduleState.module);
-                // let webpackConfigObject = createWebpackConfig(moduleState.entry, moduleObj);
-                //console.log(JSON.stringify(webpackConfigObject));
-                //converting the object created using function with inputs from front end
-                //Uint8Array.from - will convert object to to Uint8 array so we can write to a file
-                //let Uint8Obj= Uint8Array.from(webpackConfig)
-                //console.log('uint8', Uint8Obj);
+                    // let moduleObj = createModule(moduleState.module);
+                    // let webpackConfigObject = createWebpackConfig(moduleState.entry, moduleObj);
+                    //console.log(JSON.stringify(webpackConfigObject));
+                    //converting the object created using function with inputs from front end
+                    //Uint8Array.from - will convert object to to Uint8 array so we can write to a file
+                    let readUri = "/Users/lola/Documents/codesmith/aliens-vscode/webpack.config.js";
+                    //the webpack.config file need to be in the root directory but can have a different
+                    //name as long as specified when using the webpack command
+                    let writeUri = "/Users/lola/Documents/codesmith/CJOR/nimble/storage.config.js";
+                    vscode_1.workspace.fs.readFile(vscode_uri_1.URI.file(readUri))
+                        .then(res => {
+                        console.log('read', res);
+                        return vscode_1.workspace.fs.writeFile(vscode_uri_1.URI.file(writeUri), res)
+                            .then(res => {
+                            console.log('write', res);
+                            return exec('npx webpack --config storage.config.js', { cwd: __dirname });
+                        });
+                    });
                 //using workspace.writeFile (path, Uint8 array)
-                //workspace.fs.writeFile(__dirname, Uint8Obj)
                 /*write webpackConfigObject to path: __dirname (refers to where the extension is installed)
-                    .then(res => exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
-                */
-                case 'stats':
-                    console.log('getting stats');
+                                        .then(res => exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
+                                    */
+                case "stats":
+                    console.log(__dirname);
             }
         });
     });
@@ -138,23 +69,23 @@ function getWebviewContent(context) {
 		<script>
 		const vscode = acquireVsCodeApi();
 		</script>
-		${loadScript(context, 'out/nimble.js')}
+		${loadScript(context, "out/nimble.js")}
 	</body>
 	</html>`;
 }
-// webpack config functions: 
+// webpack config functions:
 // entry - moduleState.entry:
 function createWebpackConfig(entry, mod) {
     const moduleExports = {};
     moduleExports.entry = {
-        main: entry,
+        main: entry
     };
     moduleExports.output = {
-        filename: 'bundle.js',
-        path: 'path'
+        filename: "bundle.js",
+        path: "path"
     };
     moduleExports.resolve = {
-        extensions: ['.js', '.ts', '.tsx', '.json']
+        extensions: [".js", ".ts", ".tsx", ".json"]
     };
     moduleExports.module = mod;
     return moduleExports;
@@ -166,37 +97,39 @@ function createModule(modules) {
     if (modules.css) {
         module.rules.push({
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
+            use: ["style-loader", "css-loader"]
         });
     }
     if (modules.jsx) {
         module.rules.push({
             test: /\.(js|jsx)$/,
-            use: [{
-                    loader: 'babel-loader',
-                    options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
-                }],
-            exclude: '/node_modules/'
+            use: [
+                {
+                    loader: "babel-loader",
+                    options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+                }
+            ],
+            exclude: "/node_modules/"
         });
     }
     //if statement for modules.tsx
     if (module.tsx) {
         module.rules.push({
             test: /\.tsx?$/,
-            use: ['ts-loader'],
+            use: ["ts-loader"],
             exclude: /node_modules/
         });
     }
     if (module.less) {
         module.rules.push({
             test: /\.less$/,
-            loader: 'less-loader',
+            loader: "less-loader" // compiles Less to CSS
         });
     }
     if (module.sass) {
         module.rules.push({
             test: /\.s[ac]ss$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
+            use: ["style-loader", "css-loader", "sass-loader"]
         });
     }
     return module;
--- nimble/src/extension.ts
+++ nimble/src/extension.ts
@@ -1,140 +1,82 @@
 // import * as vscode from 'vscode';
-import  { ExtensionContext, commands, window, ViewColumn, Uri, workspace } from 'vscode';
+import {
+  ExtensionContext,
+  commands,
+  window,
+  ViewColumn,
+  Uri,
+  workspace
+} from "vscode";
 
 //node docs;
-const {exec} = require('child_process');
-import * as path from 'path';
-
-// let webpackConfig :any = {
-//     entry: {
-//         aliens: './src/view/index.tsx'
-//     },
-//     output: {
-//         path: path.resolve(__dirname, 'out'),
-//         filename: "[name].js"
-//     },
-//     optimization: {
-//         splitChunks: {
-//             cacheGroups: {
-//                 vendor: {
-//                     test: /node_modules/,
-//                     chunks: "initial",
-//                     name: "vendor",
-//                     priority: 10,
-//                     enforce: true
-//                 }
-//             }
-//         }
-//     },
-//     devtool: 'eval-source-map',
-//     resolve: {
-//         extensions: ['.js', '.ts', '.tsx', '.json']
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.(ts|tsx)$/,
-//                 loader: 'ts-loader',
-//                 options: {
-//                     getCustomTransformers: () => ({
-//                         before: [ tsImportPlugin({
-//                           libraryName: 'antd',
-//                           libraryDirectory: 'es',
-//                           style: true,
-//                         }) ]
-//                      })
-//                 }
-//             },
-//             {
-//                 test: /\.less$/,
-//                 use: [
-//                     {
-//                         loader: 'style-loader'
-//                     },
-//                     {
-//                         loader: 'css-loader',
-//                         options: {
-//                             importLoaders: 1,
-//                             sourceMap: true
-//                         }
-//                     },
-//                     {
-//                         loader: 'less-loader',
-//                         options: {
-//                             javascriptEnabled: true,
-//                             sourceMap: true,
-//                             modifyVars: {
-//                                 '@body-background': 'var(--background-color)',
-//                             }
-//                         }
-//                     }
-//                 ]
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     {
-//                         loader: 'style-loader'
-//                     },
-//                     {
-//                         loader: 'css-loader'
-//                     }
-//                 ]
-//             }
-//         ]
-//     },
-//     performance: {
-//         hints: false
-//     }
-// }
+const { exec } = require("child_process");
+import * as path from "path";
+import { URI } from "vscode-uri";
 
 function loadScript(context: ExtensionContext, path: string) {
-    return `<script src="${Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource'}).toString()}"></script>`;
+  return `<script src="${Uri.file(context.asAbsolutePath(path))
+    .with({ scheme: "vscode-resource" })
+    .toString()}"></script>`;
 }
 
 export function activate(context: ExtensionContext) {
-	console.log('Congratulations, your extension "nimble" is now active!');
-	// exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
-		let startCommand = commands.registerCommand('extension.startNimble', () => {
-		const panel = window.createWebviewPanel('nimble', 'Nimble', ViewColumn.Beside, {enableScripts: true,});
-		panel.webview.html = getWebviewContent(context);
-		
-		panel.webview.onDidReceiveMessage(message => {
-			console.log(message.command)
-			let moduleState: any;
-				switch(message.command) {
-					case 'config':
-						console.log('getting input and configuring webpack');
-						console.log('message', message.module)
-						moduleState = {
-							...message.module
-						};
-						// let moduleObj = createModule(moduleState.module);
-						// let webpackConfigObject = createWebpackConfig(moduleState.entry, moduleObj);
-						//console.log(JSON.stringify(webpackConfigObject));
+  console.log('Congratulations, your extension "nimble" is now active!');
+  // exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
+  let startCommand = commands.registerCommand("extension.startNimble", () => {
+    const panel = window.createWebviewPanel(
+      "nimble",
+      "Nimble",
+      ViewColumn.Beside,
+      { enableScripts: true }
+    );
+    panel.webview.html = getWebviewContent(context);
 
+    panel.webview.onDidReceiveMessage(message => {
+      console.log(message.command);
+      let moduleState: any;
+      switch (message.command) {
+        case "config":
+          console.log("getting input and configuring webpack");
+          console.log("message", message.module);
+          moduleState = {
+            ...message.module
+          };
+          // let moduleObj = createModule(moduleState.module);
+          // let webpackConfigObject = createWebpackConfig(moduleState.entry, moduleObj);
+          //console.log(JSON.stringify(webpackConfigObject));
 
-				//converting the object created using function with inputs from front end
-				//Uint8Array.from - will convert object to to Uint8 array so we can write to a file
-				//let Uint8Obj= Uint8Array.from(webpackConfig)
-				//console.log('uint8', Uint8Obj);
-				//using workspace.writeFile (path, Uint8 array)
-				//workspace.fs.writeFile(__dirname, Uint8Obj)
-							/*write webpackConfigObject to path: __dirname (refers to where the extension is installed)
+          //converting the object created using function with inputs from front end
+          //Uint8Array.from - will convert object to to Uint8 array so we can write to a file
+          let readUri =
+            "/Users/lola/Documents/codesmith/aliens-vscode/webpack.config.js";
+          //the webpack.config file need to be in the root directory but can have a different
+          //name as long as specified when using the webpack command
+            let writeUri =
+            "/Users/lola/Documents/codesmith/CJOR/nimble/storage.config.js";
+          workspace.fs.readFile(URI.file(readUri))
+            .then(res => {
+              console.log('read',res)
+            return workspace.fs.writeFile(URI.file(writeUri), res)
+              .then(res =>{
+                console.log('write',res)
+                return exec('npx webpack --config storage.config.js', {cwd: __dirname})
+              })
+          });
+        //using workspace.writeFile (path, Uint8 array)
+
+        /*write webpackConfigObject to path: __dirname (refers to where the extension is installed)
 								.then(res => exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
 							*/
-					case 'stats' :
-						console.log('getting stats')
-						}
-		});
-	
-	
-	});
-	context.subscriptions.push(startCommand);
+        case "stats":
+          console.log(__dirname);
+      }
+    });
+  });
+  context.subscriptions.push(startCommand);
 }
 
 function getWebviewContent(context: ExtensionContext) {
-	return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
@@ -148,71 +90,72 @@ function getWebviewContent(context: ExtensionContext) {
 		<script>
 		const vscode = acquireVsCodeApi();
 		</script>
-		${loadScript(context, 'out/nimble.js')}
+		${loadScript(context, "out/nimble.js")}
 	</body>
 	</html>`;
 }
 
-// webpack config functions: 
+// webpack config functions:
 // entry - moduleState.entry:
 function createWebpackConfig(entry: any, mod: any) {
-	const moduleExports:any = {};
-	moduleExports.entry = {
-		main: entry,
-	};
-	moduleExports.output = {
-		filename: 'bundle.js',
-		path: 'path'
-	};
-	moduleExports.resolve = {
-        extensions: ['.js', '.ts', '.tsx', '.json']
-	};
-	moduleExports.module = mod;
-    return moduleExports;
+  const moduleExports: any = {};
+  moduleExports.entry = {
+    main: entry
+  };
+  moduleExports.output = {
+    filename: "bundle.js",
+    path: "path"
+  };
+  moduleExports.resolve = {
+    extensions: [".js", ".ts", ".tsx", ".json"]
+  };
+  moduleExports.module = mod;
+  return moduleExports;
 }
 
 // mod: moduleState.mod
 function createModule(modules: any) {
-	const module: any = {};
-	module.rules = [];
-	if (modules.css) {
-		module.rules.push({
-			test: /\.css$/i,
-			use: ['style-loader', 'css-loader']
-		});
-	}
-	if (modules.jsx) {
-		module.rules.push({
-			test: /\.(js|jsx)$/, 
-			use: [{
-				loader: 'babel-loader',
-				options: {presets: ['@babel/preset-env', '@babel/preset-react']}
-			}],
-			exclude: '/node_modules/'
-		});
-	}
-	//if statement for modules.tsx
-	if (module.tsx) {
-		module.rules.push({
-			test: /\.tsx?$/,
-			use: ['ts-loader'],
-			exclude: /node_modules/
-		  });
-	}
-	if (module.less) {
-		module.rules.push({
-			test: /\.less$/,
-			loader: 'less-loader', // compiles Less to CSS
-		  });
-	}
-	if (module.sass) {
-		module.rules.push({
-			test: /\.s[ac]ss$/i,
-			use: ['style-loader', 'css-loader', 'sass-loader'],
-		  });
-	}
-	return module;
+  const module: any = {};
+  module.rules = [];
+  if (modules.css) {
+    module.rules.push({
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"]
+    });
+  }
+  if (modules.jsx) {
+    module.rules.push({
+      test: /\.(js|jsx)$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+        }
+      ],
+      exclude: "/node_modules/"
+    });
+  }
+  //if statement for modules.tsx
+  if (module.tsx) {
+    module.rules.push({
+      test: /\.tsx?$/,
+      use: ["ts-loader"],
+      exclude: /node_modules/
+    });
+  }
+  if (module.less) {
+    module.rules.push({
+      test: /\.less$/,
+      loader: "less-loader" // compiles Less to CSS
+    });
+  }
+  if (module.sass) {
+    module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: ["style-loader", "css-loader", "sass-loader"]
+    });
+  }
+  return module;
 }
 
 export function deactivate() {}
-
