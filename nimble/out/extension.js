3f4fd8c8a4bc62656da759e0336b57e7b730025f
courtneywkwong <cwkwong95@gmail.com>
2019-09-17 22:38:14 -0400

    checking out changes to new branch

--- nimble/out/extension.js
+++ nimble/out/extension.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // import * as vscode from 'vscode';
 const vscode_1 = require("vscode");
 const vscode_uri_1 = require("vscode-uri");
-//node docs;
+// node docs;
 const { exec } = require('child_process');
 const fs = require('fs');
 const util = require('util');
@@ -13,8 +13,8 @@ function loadScript(context, path) {
 function activate(context) {
     console.log('Congratulations, your extension "nimble" is now active!');
     // exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
-    let startCommand = vscode_1.commands.registerCommand('extension.startNimble', () => {
-        const panel = vscode_1.window.createWebviewPanel('nimble', 'Nimble', vscode_1.ViewColumn.Beside, { enableScripts: true, });
+    const startCommand = vscode_1.commands.registerCommand('extension.startNimble', () => {
+        const panel = vscode_1.window.createWebviewPanel('nimble', 'Nimble', vscode_1.ViewColumn.Beside, { enableScripts: true });
         panel.webview.html = getWebviewContent(context);
         panel.webview.onDidReceiveMessage((message) => {
             console.log('message obj from front end is: ', message);
@@ -23,18 +23,20 @@ function activate(context) {
                 case 'config':
                     moduleState = Object.assign({}, message.module);
                     // console.log('module State is: ', moduleState);
-                    let moduleObj = createModule(moduleState);
+                    const moduleObj = createModule(moduleState);
                     // let utilModuleObj = util.inspect(moduleObj);
-                    //originally wanted to json parse this object A
+                    // originally wanted to json parse this object A
                     // let A = utilModuleObj
                     // let thisPageURI = URI.file('/Users/jackie/Documents/Codesmith/production-project-sept-2019/Nimble/CJOR/nimble/src/extension.ts');
                     // let webpackConfigObject: any = createWebpackConfig(message.entry, utilModuleObj)
-                    let webpackConfigObject = createWebpackConfig(message.entry, moduleObj);
+                    const webpackConfigObject = createWebpackConfig(`${vscode_1.workspace.workspaceFolders[0].uri + message.entry}`, moduleObj);
                     // .then(res => workspace.fs.readFile(thisPageURI))
                     // .then(data => console.log('dataaaa', data.toString()));
-                    console.log("this is webpackConfigObject :", webpackConfigObject);
-                    let writeUri = `${__dirname}/webpack.config.js`;
-                    vscode_1.workspace.fs.writeFile(vscode_uri_1.URI.file(writeUri), new Uint8Array(Buffer.from(util.inspect(webpackConfigObject, { depth: null }), 'utf-8')))
+                    console.log('this is webpackConfigObject :', webpackConfigObject);
+                    const writeUri = `${__dirname}/webpack.config.js`;
+                    vscode_1.workspace.fs.writeFile(vscode_uri_1.URI.file(writeUri), new Uint8Array(Buffer.from(`const path = require('path');            
+            
+            module.exports =${util.inspect(webpackConfigObject, { depth: null })}`, 'utf-8')))
                         .then(res => console.log('yayy'));
                     console.log('dirname', __dirname);
                 case 'stats':
@@ -64,7 +66,7 @@ function getWebviewContent(context) {
 	</body>
 	</html>`;
 }
-// webpack config functions: 
+// webpack config functions:
 // entry - message.entry:
 function createWebpackConfig(entry, mod) {
     const moduleExports = {};
@@ -73,10 +75,10 @@ function createWebpackConfig(entry, mod) {
     };
     moduleExports.output = {
         filename: 'bundle.js',
-        path: 'path'
+        path: 'path',
     };
     moduleExports.resolve = {
-        extensions: ['.js', '.ts', '.tsx', '.json']
+        extensions: ['.js', '.ts', '.tsx', '.json'],
     };
     moduleExports.module = mod;
     return moduleExports;
@@ -87,10 +89,10 @@ function createModule(modules) {
     module.rules = [];
     if (modules.css) {
         module.rules.push({
-            //keeping regex in string form so that we can pass it to another file
-            //we are thinking to convert the string back to a regexpression right before injecting this code into another file
+            // keeping regex in string form so that we can pass it to another file
+            // we are thinking to convert the string back to a regexpression right before injecting this code into another file
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
+            use: ['style-loader', 'css-loader'],
         });
         // console.log("test key value from module.css obj is", module.rules[0].test);
     }
@@ -99,17 +101,17 @@ function createModule(modules) {
             test: /\.(js|jsx)$/,
             use: [{
                     loader: 'babel-loader',
-                    options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
+                    options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
                 }],
-            exclude: '/node_modules/'
+            exclude: '/node_modules/',
         });
     }
-    //if statement for modules.tsx
+    // if statement for modules.tsx
     if (modules.tsx) {
         module.rules.push({
             test: /\.tsx?$/,
             use: ['ts-loader'],
-            exclude: '/node_modules/'
+            exclude: '/node_modules/',
         });
     }
     if (modules.less) {
--- nimble/src/extension.ts
+++ nimble/src/extension.ts
@@ -1,65 +1,71 @@
 // import * as vscode from 'vscode';
-import { ExtensionContext, commands, window, ViewColumn, Uri, workspace } from 'vscode';
-import {URI} from 'vscode-uri'
-//node docs;
-const {exec} = require('child_process');
+import {
+  ExtensionContext, commands, window, ViewColumn, Uri, workspace,
+} from 'vscode';
+import { URI } from 'vscode-uri';
+// node docs;
+const { exec } = require('child_process');
 const fs = require('fs');
 const util = require('util');
 
 
 function loadScript(context: ExtensionContext, path: string) {
-    return `<script src="${Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource'}).toString()}"></script>`;
+  return `<script src="${Uri.file(context.asAbsolutePath(path)).with({ scheme: 'vscode-resource' }).toString()}"></script>`;
 }
 
 export function activate(context: ExtensionContext) {
-	console.log('Congratulations, your extension "nimble" is now active!');
-	// exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
-		let startCommand = commands.registerCommand('extension.startNimble', () => {
-		const panel = window.createWebviewPanel('nimble', 'Nimble', ViewColumn.Beside, {enableScripts: true,});
-		panel.webview.html = getWebviewContent(context);
-		
-		panel.webview.onDidReceiveMessage((message: any) => {
-			console.log('message obj from front end is: ', message);
-			let moduleState: any;
-				switch(message.command) {
-					case 'config':
-						moduleState = {
-							...message.module
-						};
-						// console.log('module State is: ', moduleState);
-						let moduleObj = createModule(moduleState);
-						// let utilModuleObj = util.inspect(moduleObj);
-						//originally wanted to json parse this object A
-						// let A = utilModuleObj
+  console.log('Congratulations, your extension "nimble" is now active!');
+  // exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname});
+  const startCommand = commands.registerCommand('extension.startNimble', () => {
+    const panel = window.createWebviewPanel('nimble', 'Nimble', ViewColumn.Beside, { enableScripts: true });
+    panel.webview.html = getWebviewContent(context);
 
-						// let thisPageURI = URI.file('/Users/jackie/Documents/Codesmith/production-project-sept-2019/Nimble/CJOR/nimble/src/extension.ts');
+    panel.webview.onDidReceiveMessage((message: any) => {
+      console.log('message obj from front end is: ', message);
+      let moduleState: any;
+      switch (message.command) {
+        case 'config':
+          moduleState = {
+            ...message.module,
+          };
+          // console.log('module State is: ', moduleState);
+          const moduleObj = createModule(moduleState);
+          // let utilModuleObj = util.inspect(moduleObj);
+          // originally wanted to json parse this object A
+          // let A = utilModuleObj
 
-						// let webpackConfigObject: any = createWebpackConfig(message.entry, utilModuleObj)
-						let webpackConfigObject: any = createWebpackConfig(message.entry, moduleObj)
+          // let thisPageURI = URI.file('/Users/jackie/Documents/Codesmith/production-project-sept-2019/Nimble/CJOR/nimble/src/extension.ts');
 
-						// .then(res => workspace.fs.readFile(thisPageURI))
-						// .then(data => console.log('dataaaa', data.toString()));
-						
-						console.log("this is webpackConfigObject :", webpackConfigObject);
+          // let webpackConfigObject: any = createWebpackConfig(message.entry, utilModuleObj)
+          const webpackConfigObject: any = createWebpackConfig(`${workspace.workspaceFolders[0].uri + message.entry}`, moduleObj);
+
+          // .then(res => workspace.fs.readFile(thisPageURI))
+          // .then(data => console.log('dataaaa', data.toString()));
+
+          console.log('this is webpackConfigObject :', webpackConfigObject);
 
 
-						let writeUri =`${__dirname}/webpack.config.js`;
+          const writeUri = `${__dirname}/webpack.config.js`;
 
-						workspace.fs.writeFile(URI.file(writeUri), new Uint8Array(Buffer.from(util.inspect(webpackConfigObject,{depth: null}), 'utf-8')))
-						.then(res => console.log('yayy'));
+          workspace.fs.writeFile(URI.file(writeUri), new Uint8Array(Buffer.from(
+            `const path = require('path');            
+            
+            module.exports =${util.inspect(webpackConfigObject, { depth: null })}`, 'utf-8',
+          )))
+            .then(res => console.log('yayy'));
 
-						console.log('dirname',__dirname);
+          console.log('dirname', __dirname);
 
-					case 'stats' :
-						console.log('getting stats')
-				}
-		});
-	});
-	context.subscriptions.push(startCommand);
+        case 'stats':
+          console.log('getting stats');
+      }
+    });
+  });
+  context.subscriptions.push(startCommand);
 }
 
 function getWebviewContent(context: ExtensionContext) {
-	return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
@@ -78,68 +84,68 @@ function getWebviewContent(context: ExtensionContext) {
 	</html>`;
 }
 
-// webpack config functions: 
+// webpack config functions:
 // entry - message.entry:
 function createWebpackConfig(entry: any, mod: any) {
-	const moduleExports:any = {};
-	moduleExports.entry = {
-		main: entry,
-	};
-	moduleExports.output = {
-		filename: 'bundle.js',
-		path: 'path'
-	};
-	moduleExports.resolve = {
-        extensions: ['.js', '.ts', '.tsx', '.json']
-	};
-	moduleExports.module = mod;
-    return moduleExports;
+  const moduleExports: any = {};
+  moduleExports.entry = {
+    main: entry,
+  };
+  moduleExports.output = {
+    filename: 'bundle.js',
+    path: 'path',
+  };
+  moduleExports.resolve = {
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+  };
+  moduleExports.module = mod;
+  return moduleExports;
 }
 
 // mod: moduleState.mod
 function createModule(modules: any) {
-	const module: any = {};
-	module.rules = [];
-	if (modules.css) {
-		module.rules.push({
-			//keeping regex in string form so that we can pass it to another file
-			//we are thinking to convert the string back to a regexpression right before injecting this code into another file
-			test: /\.css$/i,
-			use: ['style-loader', 'css-loader']
-		});
-		// console.log("test key value from module.css obj is", module.rules[0].test);
-	}
-	if (modules.jsx) {
-		module.rules.push({
-			test: /\.(js|jsx)$/, 
-			use: [{
-				loader: 'babel-loader',
-				options: {presets: ['@babel/preset-env', '@babel/preset-react']}
-			}],
-			exclude: '/node_modules/'
-		});
-	}
-	//if statement for modules.tsx
-	if (modules.tsx) {
-		module.rules.push({
-			test: /\.tsx?$/,
-			use: ['ts-loader'],
-			exclude: '/node_modules/'
+  const module: any = {};
+  module.rules = [];
+  if (modules.css) {
+    module.rules.push({
+      // keeping regex in string form so that we can pass it to another file
+      // we are thinking to convert the string back to a regexpression right before injecting this code into another file
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    });
+    // console.log("test key value from module.css obj is", module.rules[0].test);
+  }
+  if (modules.jsx) {
+    module.rules.push({
+      test: /\.(js|jsx)$/,
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+      }],
+      exclude: '/node_modules/',
+    });
+  }
+  // if statement for modules.tsx
+  if (modules.tsx) {
+    module.rules.push({
+      test: /\.tsx?$/,
+      use: ['ts-loader'],
+      exclude: '/node_modules/',
 		  });
-	}
-	if (modules.less) {
-		module.rules.push({
-			test: /\.less$/,
-			loader: 'less-loader', // compiles Less to CSS
+  }
+  if (modules.less) {
+    module.rules.push({
+      test: /\.less$/,
+      loader: 'less-loader', // compiles Less to CSS
 		  });
-	}
-	if (modules.sass) {
-		module.rules.push({
-			test: /\.s[ac]ss$/i,
-			use: ['style-loader', 'css-loader', 'sass-loader'],
+  }
+  if (modules.sass) {
+    module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
 		  });
-	}
-	return module;
+  }
+  return module;
 }
 
 export function deactivate() {}
