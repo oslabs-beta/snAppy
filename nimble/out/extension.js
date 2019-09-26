cd4d721c81c1a2ed597e9e8685f53d5d598206ac
onaumova <leliknaum@gmail.com>
2019-09-25 20:29:12 -0400

    created a insertion function for dynamic imports and tried testing. some issue with the insertion
    Co-authored-by: courtneywkwong <cwkwong95@gmail.com>

--- nimble/out/extension.js
+++ nimble/out/extension.js
@@ -49,7 +49,20 @@ module.exports =${util.inspect(webpackConfigObject, { depth: null })}`, 'utf-8')
                     //create a test readFile function from one of the component files (RR container)
                     //once read .then the variable readURI get updated with URI of current file
                     let currURI = vscode_uri_1.URI.file('/Users/lola/Documents/codesmith/soloproject/src/client/containers/RRContainer.jsx');
-                    optimize.uncommentFunc(currURI, 10);
+                    optimize.uncommentFunc(currURI, [10, 11, 12, 13]);
+                    const object = {
+                        Mantra: {
+                            name: 'Mantra',
+                            path: './Mantra',
+                        },
+                        Login: {
+                            name: 'Login',
+                            path: './Login',
+                        }
+                    };
+                    console.log("still inside");
+                    let dynamicInjection = optimize.createDynamicInjection(object);
+                    optimize.insertFunc(currURI, 88, dynamicInjection);
                     break;
                 /*
                   jackie and rachel's parsing algo for folders => ./path that requires opt();
--- nimble/src/functions/optimizeFunctions.ts
+++ nimble/src/functions/optimizeFunctions.ts
@@ -1,10 +1,64 @@
 import { WorkspaceEdit, workspace, Position } from "vscode";
+import { string } from "prop-types";
 
-export const uncommentFunc = (uri: any, line: number) => {
+export const uncommentFunc = (uri: any, lines: number[]) => {
   //will use that and the starting position to comment out static imports by using workspaceEdit.insert(URI, position, string)
-  let edit = new WorkspaceEdit();
-  console.log("line", line);
-  edit.insert(uri, new Position(line-1, 0), "//");
+console.log("length", lines.length)
+let edit = new WorkspaceEdit();
+for (let line of lines) {
+    edit.insert(uri, new Position(line-1, 0), "//");
+   }
   workspace.applyEdit(edit)
-  .then(res => console.log("edited", res));
+  .then(res => console.log("edited", res))
 };
+
+export const createDynamicInjection = (componentObject: any) => {
+  console.log("inside dynamic import")
+  //an outside function that loops through the object and for each key will execute the function below to create a new instance of class
+  //will have a varibale "injection"  with the class declaration
+  let injection = `class DynamicImport extends Component {
+    state = {
+      component: null
+    }
+    componentDidMount() {
+      this.props.load()
+        .then((component) => {
+          this.setState(()=> ({
+            component : component.default ? component.default : component
+          }))
+      })
+    }
+    render () {
+      return this.props.children(this.state.component)
+    }
+  }
+  `
+    for (let val in componentObject) {
+      console.log(componentObject[val].name)
+      injection += newInstance(componentObject[val].name, componentObject[val].path)
+    }
+  //concatenate every new instance of class invoked with each key/values to injection string
+  //return the resulting string with injection+ each instance of new class
+    return injection
+  }
+  //inside of the outside func there will be a function that injects a string literal with values from the object into string declaration of new instance of DynamicImports class
+  function newInstance(name:string, path: string) {
+    return `const ${name} = (props) => (
+    <DynamicImport load= {() => import(/*wepbackChunkName: "${name}-chunk"*/ '${path}')}>{
+      (Component) => Component === null 
+      ? <p>Loading..</p>
+      : <Component {...props}/>
+    }</DynamicImport>
+  )
+  `
+  }
+
+  export const insertFunc =(uri: any, line: number, injection: string) => {
+    console.log("inject", injection)
+    let edit = new WorkspaceEdit();
+    edit.insert(uri, new Position (line-2,0), injection)
+      // .then(res => console.log('is it inserting?', res))
+    workspace.applyEdit(edit)
+      .then(res => console.log("applyedit",res))
+
+  }
