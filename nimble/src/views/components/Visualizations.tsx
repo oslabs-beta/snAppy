808e94ce39783d93b81f1ea042ee5c16ec381e77
courtneywkwong <cwkwong95@gmail.com>
2019-09-29 16:40:34 -0400

    created vis component to import liquid guage comp

--- nimble/src/views/components/Visualizations.tsx
+++ nimble/src/views/components/Visualizations.tsx
@@ -1,4 +1,14 @@
 import * as React from 'react';
+import LiquidGauges from './d3/LiquidGauges';
 
-const Visualizations = () => <div>Hello</div>;
+class Visualizations extends React.Component {
+    render() {
+        return (
+            <div>
+                <LiquidGauges/>
+            </div>
+        );
+    }
+}
+
 export default Visualizations;
