14063182b4df14fd36dbe47df27e97898be61c0f
courtneywkwong <cwkwong95@gmail.com>
2019-09-29 18:49:52 -0400

    rendering main bundle stats

--- nimble/src/views/components/d3/LiquidGauges.tsx
+++ nimble/src/views/components/d3/LiquidGauges.tsx
@@ -11,7 +11,12 @@ interface Props {
     postBundleStats: Asset[];
 }
 
+interface MainBundle {
+    initial?: number;
+    post: number;
+}
 const LiquidGauges: React.FC<Props> = (props) => {
+    // const [liquidFill, setliquidFill] = React.useState()
     React.useEffect(()=>{
         const config = liquidFillGaugeDefaultSettings();
         config.circleThickness = 0.1;
@@ -28,12 +33,26 @@ const LiquidGauges: React.FC<Props> = (props) => {
         config.waveOffset = 0.25;
         config.textSize = 0.75;
         config.waveCount = 3;
-        if (props.initialBundleStats[0].name === 'bundle.js' || props.postBundleStats[0].name === 'bundle.js') {
-            console.log(props.initialBundleStats[0].size, props.postBundleStats[0].size); 
-        }  
-        loadLiquidFillGauge("fillgauge1",60.44, config);
+        const mainBundle = getMainSize(props.initialBundleStats, props.postBundleStats);
+        loadLiquidFillGauge("fillgauge1",mainBundle.initial, config, mainBundle.post);
     });
     return <svg id="fillgauge1" width="19%" height="200"></svg>;
 };
-
+function getMainSize(initial: Asset[], post: Asset[]) {
+    //iterate thru initial and post - and only use where nane = 'bundle.js'
+    let initialN=0;
+    let postN = 0;
+    for (let obj of initial) {
+        if (obj.name === 'bundle.js') {
+            initialN = obj.size;
+        }
+    }
+    for (let obj of post) {
+        if (obj.name === 'bundle.js') {
+            postN = obj.size;
+        }
+    }
+    const mainObjs: MainBundle = { initial: initialN, post: postN };
+    return mainObjs;
+}
 export default LiquidGauges;
