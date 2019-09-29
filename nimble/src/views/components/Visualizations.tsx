c8c47c63aea3d212a47fd57710152bb7d966eeca
courtneywkwong <cwkwong95@gmail.com>
2019-09-29 17:12:29 -0400

    created interface for props of initial and post stats

--- nimble/src/views/components/Visualizations.tsx
+++ nimble/src/views/components/Visualizations.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react';
 import LiquidGauges from './d3/LiquidGauges';
 
-class Visualizations extends React.Component {
+interface Asset {
+    name: string;
+    size: number;
+    chunks: number[];
+    chunkNames: string[];
+}
+
+interface Props {
+    initialBundleStats: Asset[];
+    postBundleStats: Asset[];
+}
+
+class Visualizations extends React.Component <Props, {}> {
+    constructor(props: any) {
+        super(props);
+    }
     render() {
+        const {initialBundleStats, postBundleStats} = this.props;
+        if (initialBundleStats[0].name === 'bundle.js' || postBundleStats[0].name === 'bundle.js') {
+            console.log(initialBundleStats[0].size, postBundleStats[0].size); 
+        }
         return (
             <div>
                 <LiquidGauges/>
