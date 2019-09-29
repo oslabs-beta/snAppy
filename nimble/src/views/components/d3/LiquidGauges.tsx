c20bce1733c5b0f5769487c1ae93ea949cda121f
courtneywkwong <cwkwong95@gmail.com>
2019-09-29 19:38:41 -0400

    toggle works, moved onclick outside into jsx onClick

--- nimble/src/views/components/d3/LiquidGauges.tsx
+++ nimble/src/views/components/d3/LiquidGauges.tsx
@@ -12,11 +12,14 @@ interface Props {
 }
 
 interface MainBundle {
-    initial?: number;
+    initial: number;
     post: number;
 }
+
 const LiquidGauges: React.FC<Props> = (props) => {
-    // const [liquidFill, setliquidFill] = React.useState()
+    const [toggle, setToggle] = React.useState(true);
+    const [bundle, setBundle] = React.useState({initial:0, post:0});
+    const [myGauge, setGauge] = React.useState({update(num:number){console.log(num);}});
     React.useEffect(()=>{
         const config = liquidFillGaugeDefaultSettings();
         config.circleThickness = 0.1;
@@ -35,12 +38,17 @@ const LiquidGauges: React.FC<Props> = (props) => {
         config.waveCount = 3;
         const mainBundle = getMainSize(props.initialBundleStats, props.postBundleStats);
         const gauge = loadLiquidFillGauge("fillgauge1",mainBundle.initial, config, mainBundle.post);
-        document.getElementById('fillgauge1')!.onclick=()=>{
-            console.log('on click CLICKED');
-            gauge.update(mainBundle.post);
-        };
+        setGauge(gauge);
+        setBundle(mainBundle);
+        // document.getElementById('fillgauge1')!.onclick=()=>{
+            
+        // };
     }, []);
-    return <svg id="fillgauge1" width="19%" height="200"></svg>;
+    return <svg id="fillgauge1" width="19%" height="200" onClick={()=>{
+        console.log('on click CLICKED', toggle);
+        myGauge.update(toggle ? 777 : bundle.initial);
+        setToggle(!toggle);
+    }}></svg>;
 };
 function getMainSize(initial: Asset[], post: Asset[]) {
     //iterate thru initial and post - and only use where nane = 'bundle.js'
@@ -51,7 +59,6 @@ function getMainSize(initial: Asset[], post: Asset[]) {
             initialN = obj.size;
         }
     }
-
     for (let obj of post) {
         if (obj.name === 'bundle.js') {
             postN = obj.size;
