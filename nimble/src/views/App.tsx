c1c92aae66e0fd28dd3689db112437ea303b849c
onaumova <leliknaum@gmail.com>
2019-09-30 09:14:59 -0400

    added gifs between each bundling

--- nimble/src/views/App.tsx
+++ nimble/src/views/App.tsx
@@ -92,7 +92,8 @@ export default class App extends React.Component<{},State> {
          
         }
         if (this.state.bundleButtonClicked) {
-            CurrentComponent= <div>Caaaat Cooooding!</div>
+            CurrentComponent= <div><img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" /> <br></br>
+            Caaaat Cooooding!</div>
         }
         if (this.state.initialBundleComplete && this.state.initialBundleStats) {
             CurrentComponent = 
@@ -100,8 +101,13 @@ export default class App extends React.Component<{},State> {
             <Assets initialBundleStats={this.state.initialBundleStats} optFunc = {optimize} entry={this.state.entry} />
             </div>
         }
+        
         if (this.state.optimizeButtonClicked) {
-            CurrentComponent = <div>Snapping...!</div>
+            CurrentComponent = <div>
+            <img src="https://cdn.dribbble.com/users/2063732/screenshots/6330750/untitled-1.gif" width="300" />
+            <br></br>
+            <img src ="https://images2.minutemediacdn.com/image/upload/c_crop,h_674,w_1200,x_0,y_113/f_auto,q_auto,w_1100/v1554999553/shape/mentalfloss/504605-publicdomain.gif"  width= "300"></img>
+            </div>
         }
         if (this.state.postBundleComplete && this.state.postBundleStats) {
             CurrentComponent = 
