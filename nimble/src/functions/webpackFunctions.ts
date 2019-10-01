87a38c30f788a455029e396121605c4c94b820dc
courtneywkwong <cwkwong95@gmail.com>
2019-09-30 22:05:49 -0400

    stashing changes to new branch

--- nimble/src/functions/webpackFunctions.ts
+++ nimble/src/functions/webpackFunctions.ts
@@ -1,6 +1,7 @@
 import { window, workspace } from "vscode";
 import { URI } from 'vscode-uri';
 import util = require('util');
+import path = require('path');
 const { exec } = require('child_process');
 
 export const runWriteWebpackBundle = (moduleStateObj: any, panel: any) => {
@@ -8,16 +9,16 @@ export const runWriteWebpackBundle = (moduleStateObj: any, panel: any) => {
           // console.log(workspace.workspaceFolders? workspace.workspaceFolders[0]: '/', 'message.entry:', message.entry);
           const webpackConfigObject: any = createWebpackConfig(`${(workspace.workspaceFolders? workspace.workspaceFolders[0].uri.path : '/') + moduleStateObj.entry}`, moduleObj);
           console.log('this is webpackConfigObject :', webpackConfigObject);
-          const writeUri = `${__dirname}/webpack.config.js`;
+          const writeUri = path.join(__dirname, '..', 'webpack.config.js');
           workspace.fs.writeFile(URI.file(writeUri), new Uint8Array(Buffer.from(
             `const path = require('path');
               module.exports =${util.inspect(webpackConfigObject, { depth: null })}`, 'utf-8',
           )))
             .then(res => {
               window.showInformationMessage('Bundling...');
-              return exec('npx webpack --profile --json > compilation-stats.json', {cwd: __dirname}, (err : Error, stdout: string)=>{
+              return exec('npx webpack --profile --json > compilation-stats.json', {cwd:  path.join(__dirname, '..')}, (err : Error, stdout: string)=>{
 
-                workspace.fs.readFile(URI.file(`${__dirname}/compilation-stats.json`))
+                workspace.fs.readFile(URI.file(path.join(__dirname, '..', 'compilation-stats.json')))
                   .then(res => {
                   return  panel.webview.postMessage({command: 'initial', field: res.toString()});
                   });
