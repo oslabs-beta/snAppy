332e5212beee20918adce1611bba4c9478180abf
Rachel Park <vinetree@gmail.com>
2019-10-01 20:10:10 -0400

    tried to set up jest and solve the conflict issue with mocha test setup
    Co-authored-by: jackiel12 <jackie.lin128@gmail.com>

--- nimble/src/test/suite/extension.test.ts
+++ nimble/src/test/suite/extension.test.ts
@@ -1,18 +1,11 @@
 import * as assert from 'assert';
-import { before } from 'mocha';
-
+import * as jest from 'jest';
+import { runCLI } from 'jest-cli';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
-suite('Extension Test Suite', () => {
-	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
-	});
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
-});
+//jest testing begins here
+
