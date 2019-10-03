85831a50939d897aef2a9f1f03d2b31d7ec663d4
Jackie Lin <jackie.lin128@gmail.com>
2019-10-03 10:45:41 -0400

    moved files out of the nimber folder into the root directory

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# snAppy
-VS Code exntension for Front End Optimization using dynamic imports and bundling your React application using webpack!
+# README
 
-Welcome to our Git Repository!
+snAppy is a VS Code extension coupled with an interactive view to support your React front-end delivery. It automates dynamic and vendor code-splitting all within your workspace.  
+
+## Features
+
+snAppy includes Webpack configuration and bundling, automated dynamic/vendor code-splitting, and exporting of webpack.config.js and bundle report files. snAppy works on top of your code base, never deleting your code and you may choose to keep/delete any changes made. 
+
+(gif/demo coming soon)
+
+## Extension Settings
+
+Pull down the Command Palette and search: snAppy: Start on Current Workspace
+
+Turn on auto-save to allow for rebundle of your application after optimizations have been made. You may still undo any changes afterwards. 
+
+## Release Notes
+
+Created by: Courtney Kwong, Jackie Lin, Olga Naumova, Rachel Park
+
+### 0.5.0
+
+Initial release of snAppy. More to come! 
+
---
+++ out/functions/optimizeFunctions.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode_1 = require("vscode");
+function dynamicImportFunc(uri, uncommentLines, exportLine, components) {
+    //will use that and the starting position to comment out static imports by using workspaceEdit.insert(URI, position, string)
+    let edit = new vscode_1.WorkspaceEdit();
+    for (let line of uncommentLines) {
+        edit.insert(uri, new vscode_1.Position(line - 1, 0), "//");
+    }
+    vscode_1.workspace.applyEdit(edit).then(res => {
+        console.log("edited", res);
+        let dynamicInjection = createDynamicInjection(components);
+        insertFunc(uri, exportLine, dynamicInjection);
+    });
+}
+exports.default = dynamicImportFunc;
+const createDynamicInjection = (componentObject) => {
+    //an outside function that loops through the object and for each key will execute the function below to create a new instance of class
+    //will have a varibale "injection"  with the class declaration
+    let injection = `class DynamicImport extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        component: null
+      }
+    }
+    componentDidMount() {
+      this.props.load()
+        .then((component) => {
+          this.setState(()=> ({
+            component : component.default ? component.default : component
+          }))
+      })
+    }
+    render () {
+      return this.props.children(this.state.component)
+    }
+  }
+
+  `;
+    for (let val in componentObject) {
+        console.log(componentObject[val].name);
+        injection += newInstance(componentObject[val].name, componentObject[val].source);
+    }
+    //concatenate every new instance of class invoked with each key/values to injection string
+    //return the resulting string with injection+ each instance of new class
+    return injection;
+};
+//inside of the outside func there will be a function that injects a string literal with values from the object into string declaration of new instance of DynamicImports class
+function newInstance(name, path) {
+    return `const ${name} = (props) => (
+    <DynamicImport load= {() => import(/*webpackChunkName: "${name}-chunk"*/ '${path}')}>{
+      (Component) => Component === null 
+      ? <p>Loading..</p>
+      : <Component {...props}/>
+    }</DynamicImport>
+  )
+  `;
+}
+const insertFunc = (uri, line, injection) => {
+    console.log("inject", injection);
+    console.log("line", line);
+    let edit = new vscode_1.WorkspaceEdit();
+    edit.insert(uri, new vscode_1.Position(line - 1, 0), injection);
+    // .then(res => console.log('is it inserting?', res))
+    vscode_1.workspace.applyEdit(edit).then(res => console.log("applyedit", res));
+};
+//# sourceMappingURL=optimizeFunctions.js.map
