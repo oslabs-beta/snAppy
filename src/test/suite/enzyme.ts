17310a59e70b01d0c46eb8b78a2b4d7c0e07386f
courtneywkwong <cwkwong95@gmail.com>
2019-10-23 15:48:32 -0400

    imported components
    Co-authored-by: jackiel12 <jackie.lin128@gmail.com>
    Co-authored-by: onaumova <leliknaum@gmail.com>

--- src/test/suite/enzyme.ts
+++ src/test/suite/enzyme.ts
@@ -1,4 +1,10 @@
 import {configure, shallow} from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import App from '../../views/App';
+import Assets from '../../views/components/Assets';
+import Form from '../../views/components/Form';
+import Visualization from '../../views/components/Visualizations';
+import LiquidGauge from '../../views/components/visuals/LiquidGauges';
+
 
 configure({ adapter: new Adapter() });
