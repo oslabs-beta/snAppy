a85d42194425446b9ea11742abbe396fc5e08601
courtneywkwong <cwkwong95@gmail.com>
2019-10-23 18:05:40 -0400

    mocked functions for form component - passed
    Co-authored-by: jackiel12 <jackie.lin128@gmail.com>
    Co-authored-by: onaumova <leliknaum@gmail.com>

--- src/__test__/enzyme.test.tsx
+++ src/__test__/enzyme.test.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import {configure, shallow} from 'enzyme';
+import {configure, shallow, ShallowWrapper} from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import * as toJson from 'enzyme-to-json';
 import Assets from '../views/components/Assets';
 import Form from '../views/components/Form';
 import Visualization from '../views/components/Visualizations';
 import LiquidGauge from '../views/components/visuals/LiquidGauges';
-import { JSXElement } from '@babel/types';
 
 import App from '../views/App';
 
 configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-  let wrapper: any;
+  let wrapper: ShallowWrapper;
   beforeAll(()=>{
     wrapper = shallow(<App/>);  
   })
@@ -22,8 +22,41 @@ describe('<App />', () => {
   it('renders one h1', () => {
      expect(wrapper.find('h1').length).toEqual(1);
    }); 
-    it('h1 renders logo in text', () => {
-      expect(wrapper.find('h1').text()).toEqual('snAppy');
+  it('h1 renders logo in text', () => {
+    expect(wrapper.find('h1').text()).toEqual('snAppy');
+  });
+});
+
+const clickForm = jest.fn();
+
+describe('<Form/>', () => {
+  interface Props {
+    runFunc: Function;
+    entryFunc: Function;
+    entry: string;
+  }
+  let wrapper: ShallowWrapper;
+  const runWebpackGetStats = jest.fn();
+  const entryFunc = jest.fn();
+  const entry = 'mock string';
+
+  const props = {
+    runFunc: runWebpackGetStats,
+    entryFunc: entryFunc,
+    entry: entry
+  };
+  beforeAll(()=> {
+    wrapper=shallow(<Form {...props}/>);
+  });
+    it('is a div with a form', () => {
+      expect(wrapper.find('form').length).toEqual(1);
+    });
+    it('has an onClick function', () => {
+      const fakeEvent = { preventDefault: () => console.log('preventDefault') };
+      wrapper
+        .find('form')
+        .simulate('submit', fakeEvent);
+      expect(runWebpackGetStats).toHaveBeenCalled();
     });
 });
 
